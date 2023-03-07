a69ae7eb7aeb29b1917ea174f98d2b9f8a3ef4e9
MikeGolf07 <92555316+MikeGolf07@users.noreply.github.com>
2023-03-07 09:15:53 -0300

    Initial commit

---
+++ JS/function.js
@@ -0,0 +1,13 @@
+function correo(email) {
+    alert(email);
+}
+
+function telefono(numero) {
+    alert(numero)
+}
+function direccion(calle) {
+    alert(calle)
+}
+correo("mikegolf07@gmail.com");
+telefono("+549114272725");
+direccion("Necochea 1045");
